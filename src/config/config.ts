eee1f3b90298ae9367e74cf559c18b876538fb54
Brian Baliach <brianbaliach@Brians-MacBook-Pro.local>
2023-10-12 18:14:13 +0300

    minor bug fixes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 const devParams = {
   port: 4000,
   database: 'goweft_user_management',
-  database_username: 'phpmyadmin',
+  database_username: 'root',
   database_password: 'phpmyadmin',
   database_host: 'localhost',
   database_dialect: 'mariadb',
